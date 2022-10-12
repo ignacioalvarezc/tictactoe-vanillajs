59a8c1781af33520c767055bf371d53d0f6b4fda
ignacioalvarezc <82625741+ignacioalvarezc@users.noreply.github.com>
2022-10-12 17:09:53 -0300

    2 files modified, 2 deleted

---
+++ game.js
@@ -0,0 +1,22 @@
+const game = {
+  xTurn: true,
+  xState: [],
+  oState: [],
+  winningStates: [
+    // Rows
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    // Columns
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    // Diagonal
+    ['0', '1', '2'],
+    ['4', '5', '6'],
+  ]
+}
+
+document.addEventListener('click', event => {
+
+});
